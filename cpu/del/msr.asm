8da1e84f04ad23366acd39263b13de89a7b04478
unuse1ess <895348025@qq.com>
2021-04-07 14:45:28 +0800

    Reconstructed some files

--- cpu/del/msr.asm
+++
@@ -1,7 +0,0 @@
-;-------------------------------------------------------
-;						History
-;	Version 0.1:
-;		2021/2/22 Created by MYM. Offered interfaces of
-;		reading model specified registers.
-;-------------------------------------------------------
-
